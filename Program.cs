52c78865731fd440892110e260d0641feb5aca02
xmovse00 <xmovse00@vutbr.cz>
2024-04-14 15:29:45 +0200

    Upload files to "/"
    feat: added makefile, xml and Program.cs

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using SharpPcap;
+using PacketDotNet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string interfaceName = null;
+        bool showInterfaces = false;
+        bool tcp = false;
+        bool udp = false;
+        int? port = null;
+        int? sourcePort = null;
+        int? destinationPort = null;
+        bool arp = false;
+        bool ndp = false;
+        bool icmp4 = false;
+        bool icmp6 = false;
+        bool igmp = false;
+        bool mld = false;
+        int numberOfPackets = 1;
+
+        for (int i = 0; i < args.Length; i++){
+            switch (args[i]){
+                case "-i":
+                case "--interface":
+                    if (i + 1 < args.Length && !args[i + 1].StartsWith("-")) {
+                        interfaceName = args[i + 1];
+                    }
+                    else {
+                        showInterfaces = true;
+                    }
+                    break;
+                case "-p":
+                    port = int.Parse(args[i + 1]);
+                    break;
+                case "--port-source":
+                    sourcePort = int.Parse(args[i + 1]);
+                    break;
+                case "--port-destination":
+                    destinationPort = int.Parse(args[i + 1]);
+                    break;
+                case "--tcp":
+                case "-t":
+                    tcp = true;
+                    break;
+                case "--udp":
+                case "-u":
+                    udp = true;
+                    break;
+                case "--arp":
+                    arp = true;
+                    break;
+                case "--icmp4":
+                    icmp4 = true;
+                    break;
+                case "--icmp6":
+                    icmp6 = true;
+                    break;
+                case "--igmp":
+                    igmp = true;
+                    break;
+                case "--mld":
+                    mld = true;
+                    break;
+                case "-n":
+                    numberOfPackets = int.Parse(args[i + 1]);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
